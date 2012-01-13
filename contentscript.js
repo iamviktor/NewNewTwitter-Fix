3d42958550c7eea6b3b5a033b2f710b9c6c4f584
Viktor Kolchenko <viktor.kolchenko@gmail.com>
2012-01-13 21:21:54 +0200

    Code created.

---
+++ contentscript.js
@@ -0,0 +1,7 @@
+(function() {
+  if (document.getElementsByClassName('dashboard').length > 0 && document.getElementsByClassName('content-main').length > 0) {
+    document.getElementsByClassName('dashboard')[0].style.cssFloat = 'right';
+    document.getElementsByClassName('content-main')[0].style.cssFloat = 'left';
+  }
+}
+)();
